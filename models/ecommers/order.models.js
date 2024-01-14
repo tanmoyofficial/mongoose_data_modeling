721d16e1647571a9170aac29c9d3fab653b19529
Tanmoy <tanmoyofficial7@gmail.com>
2024-01-14 22:06:14 +0530

    Mongoose Data Modeling First commit

---
+++ models/ecommers/order.models.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const orderItemSchema = mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    orderItems: {
+      type: [ordreItemSchema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['PENDING', 'CANCELLED', 'DELIVERED'],
+      default: 'PENDING',
+    },
+  },
+  { timestamp: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
